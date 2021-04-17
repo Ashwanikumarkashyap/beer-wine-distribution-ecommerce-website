24bd9fc5b9f6c17db38d5219ec5e6be24c524b1e
Soumil Mandal <soumil.mandal@gmail.com>
2021-04-16 21:59:10 -0500

    added signup, signin and some get functions

---
+++ __init__.py
@@ -0,0 +1,208 @@
+from flask import Flask, session, g, render_template, request, make_response
+import pymongo
+import urllib.parse
+from bson.json_util import dumps
+import math
+import json
+import bcrypt
+
+app = Flask(__name__)
+
+username = urllib.parse.quote("admin")
+password = urllib.parse.quote("admin")
+url = "mongodb+srv://" + username + ":" + password + "@cluster0.riul3.mongodb.net/beer_wine_website?retryWrites=true&w=majority"
+cluster = pymongo.MongoClient(url)
+db = cluster["beer_wine_website"]
+
+# PAGE RENDERS
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route("/sign_up")
+def sign_up():
+    return render_template('signup.html')
+
+@app.route("/sign_in")
+def sign_in():
+    return render_template('signin.html')
+
+# POST FUNCTIONS
+
+@app.route("/val_sign_up", methods=["POST"])
+def val_sign_up():
+    
+    user_name = request.args.get("user_name")
+    full_name = request.args.get("full_name")
+    email_id = request.args.get("email_id")
+    password = request.args.get("password")
+    address = request.args.get("address")
+    contact_no = request.args.get("contact_no")
+    govt_id = request.args.get("govt_id")
+
+    collection = db["customer_details"]
+
+    try:
+
+        collection.insert_one({
+            "user_name": user_name,
+            "full_name": full_name,
+            "email_id": email_id,
+            "password": bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()),
+            "address": address,
+            "contact_no": contact_no,
+            "govt_id": govt_id,
+            "isAdmin": False
+        })
+        res = make_response(json.dumps({"status": "success"}))
+        res.set_cookie(
+            "data",
+            max_age=3600,
+            expires=None,
+            path=request.path,
+            domain=None,
+            secure=False
+        )
+        return res
+
+    except:
+        return json.dumps({"status": "failed"})
+
+# completed
+@app.route("/val_sign_in", methods=["POST"])
+def val_sign_in():
+
+    user_name = request.args.get("user_name")
+    password = request.args.get("password")
+
+    collection = db["customer_details"]
+
+    db_check = collection.find_one({"user_name": user_name})
+
+    if db_check and bcrypt.checkpw(password.encode("utf-8"), db_check["password"]):
+        res = make_response(json.dumps({
+            "status": "success",
+        }))
+        res.set_cookie(
+            "data",
+            max_age=3600,
+            expires=None,
+            path=request.path,
+            domain=None,
+            secure=False
+        )
+        return res
+    
+    return json.dumps({"status": "failed"})
+
+# completed
+@app.route("/add_to_products", methods=["POST"])
+def add_to_products():
+
+    brand = request.args.get("brand")
+    description = request.args.get("description")
+    stock = request.args.get("stock")
+    category = request.args.get("category")
+    price = request.args.get("price")
+    images = request.args.getlist("images")
+
+    collection = db["product_details"]
+
+    try:
+
+        collection.insert_one({
+            "brand": brand,
+            "description": description,
+            "stock": stock,
+            "category": category,
+            "price": price,
+            "images": images
+        })
+
+        return json.dumps({"status": "success"})
+
+    except:
+        return json.dumps({"status": "failed"})
+
+# POST/PUT FUNCTIONS
+
+@app.route("/add_to_wishlist", methods=["POST", "PUT"])
+def add_to_wishlist():
+    pass
+
+@app.route("/add_to_cart", methods=["POST", "PUT"])
+def add_to_cart():
+    pass
+
+
+@app.route("/rem_from_wishlist", methods=["PUT"])
+def rem_from_wishlist():
+
+    wishlist_id = request.args.get("wishlist_id")
+
+    collection = db["wishlist"]
+
+    try:
+        collection.remove({"wishlist_id": wishlist_id})
+        return json.dumps({"status": "success"})
+    except:
+        return json.dumps({"status": "failed"})
+
+@app.route("/rem_from_cart", methods=["PUT"])
+def rem_from_cart():
+
+    cart_id = request.args.get("cart_id")
+
+    collection = db["cart"]
+
+    try:
+        collection.remove({"cart_id": cart_id})
+        return json.dumps({"status": "success"})
+    except:
+        return json.dumps({"status": "failed"})
+
+
+# GET FUNCTIONS
+
+# completed (contains options to filter by category, min price, max price)
+@app.route("/get_products", methods=["GET"])
+def get_products():
+
+    category = request.args.get("category")
+    price_min = request.args.get("price_min")
+    price_max = request.args.get("price_max")
+
+    if not category:
+        category = {"$exists": True}
+    if not price_min:
+        price_min = 0
+    if not price_max:
+        price_max = math.pow(10, 4)
+
+    products = db["product_details"]
+    return dumps(list(products.find({"category": category, "price": {"$gte": price_min, "$lte": price_max}})))
+
+# completed
+@app.route("/get_cart", methods=["GET"])
+def get_cart():
+    customer_id = request.args.get("customer_id")
+    cart = db["cart"]
+    return dumps(list(cart.find({"customer_id": customer_id})))
+
+# completed
+@app.route("/get_wishlist", methods=["GET"])
+def get_wishlist():
+    customer_id = request.args.get("customer_id")
+    wishlist = db["wishlist"]
+    return dumps(list(wishlist.find({"customer_id": customer_id})))
+
+# completed
+@app.route("/get_orders", methods=["GET"])
+def get_orders():
+    customer_id = request.args.get("customer_id")
+    orders = db["orders"]
+    return dumps(list(orders.find({"customer_id": customer_id})))
+
+if __name__ == "__main__":
+    app.run()   
