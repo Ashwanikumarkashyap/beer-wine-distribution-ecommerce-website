0da6a5c19899cac1b416889ac646670d8805980f
Ruchi Singh <ruchisingh@Ruchis-Air.lan>
2021-04-28 16:33:16 -0500

    minor change in data type

--- __init__.py
+++ __init__.py
@@ -799,8 +799,8 @@ def get_products():
 
     total_documents = products.find({'name': { '$regex':  '^'+searchbox_text+'', '$options': 'i'}, "category": category, "price": {"$gt": price_min, "$lt": price_max}}).count()
     total_pages = math.ceil(total_documents/limit)
-    price_min = int(price_min)
-    price_max = int(price_max)
+    price_min = float(price_min)
+    price_max = float(price_max)
 
     cursor = products.find({'name': { '$regex':  '^'+searchbox_text+'', '$options': 'i'}, "category": category, "price": {"$gt": price_min, "$lt": price_max}}).skip(skips).limit(limit)
  
